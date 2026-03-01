557095ab65a351cbbd2c5177e319ff183b9ac6b5
PhoenixxPlays <wolfscopez69@gmail.com>
2026-03-01 12:59:58 -0500

    Update meanmedian.py

--- meanmedian.py
+++ meanmedian.py
@@ -4,3 +4,29 @@
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
 # video url: https://youtu.be/VGrRdUunjXg
+
+theList = []
+
+for i in range(5):
+    theList.append(int(input(f"Number {i+1}: ")))
+
+print("as entered: " + str(theList))
+
+theList.sort()
+
+print(f"small to large: {theList}")
+
+theList.sort(reverse=True)
+
+print(f"large to small: {theList}")
+
+median = theList[2]
+
+sum = 0
+
+for i in range(5):
+    sum += theList[i]
+
+mean = sum / 5
+
+print(f"The median is {median} and the mean is {mean}")
